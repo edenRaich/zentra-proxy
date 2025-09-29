b6808714542be70633ac8ce12291abb70aaa39d5
edenRaich <edenjraich@gmail.com>
2025-09-29 14:58:01 -0400

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 
